a359382ab911f6ebe19a230a5a8491c66c82223e
Farshid Nooshi <farshidnooshi726@aut.ac.ir>
2022-04-22 21:11:37 +0430

    update stopwords.dat for two scenarios and main.py and passed tests

--- Phase_1/main.py
+++ Phase_1/main.py
@@ -1,6 +1,9 @@
 from __future__ import unicode_literals
 
 import json
+from os import path
+import codecs
+from Phase_1.src.utils import data_path
 from hazm import *
 
 
@@ -17,4 +20,50 @@ def read_file():
     return docs_url, docs_title, docs_content
 
 
-normalizer = Normalizer()
+# coding: utf8
+
+default_stop_words = path.join(data_path, 'stopwords.dat')
+
+
+class StopWord:
+    """ Class for remove stop words
+
+         >>> StopWord().clean(["بودی؟","مشهد","در","کی"])
+         ['بودی؟', 'مشهد', 'کی']
+         >>> StopWord(normal=True).clean(["بودی؟","مشهد","در","کی"])
+         ['بودی؟', 'مشهد']
+
+         """
+
+    def __init__(self, file_path=default_stop_words, normal=False):
+        self.file_path = file_path
+        self.normal = normal
+        self.normalizer = Normalizer().normalize
+        self.stop_words = self.init(file_path, normal)
+
+    def init(self, file_path, normal):
+        if not normal:
+            return set(
+                line.strip("\r\n") for line in codecs.open(file_path, "r", encoding="utf-8").readlines())
+        else:
+            return set(
+                self.normalizer(line.strip("\r\n")) for line in
+                codecs.open(file_path, "r", encoding="utf-8").readlines())
+
+    def set_normalizer(self, func):
+        self.normalizer = func
+        self.stop_words = self.init(self.file_path, self.normal)
+
+    def __getitem__(self, item):
+        return item in self.stop_words
+
+    def __str__(self):
+        return str(self.stop_words)
+
+    def clean(self, iterable_of_strings, return_generator=False):
+        if return_generator:
+            return filter(lambda item: not self[item], iterable_of_strings)
+        else:
+            return list(filter(lambda item: not self[item], iterable_of_strings))
+
+
