ce6121df471031248085f4bd0b915cf159a6a0d3
Farshid Nooshi <farshidnooshi726@aut.ac.ir>
2022-04-22 15:29:02 +0430

    update codes and initialized workspace

--- Phase 1/main.py
+++ Phase 1/main.py
@@ -1,1 +1,20 @@
-print('hello world')
+from __future__ import unicode_literals
+
+import json
+from hazm import *
+
+
+def read_file():
+    docs_title = []
+    docs_content = []
+    docs_url = []
+    with open('assets/IR_data_news_12k.json', encoding='UTF-8') as f:
+        data = json.load(f)
+        for i in data:
+            docs_title.append(data[i]["title"])
+            docs_content.append(data[i]["content"])
+            docs_url.append(data[i]['url'])
+    return docs_url, docs_title, docs_content
+
+
+normalizer = Normalizer()
